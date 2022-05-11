44323bfe942976efc80d8b715c1d245c11969b20
Joao Moran Rosales <moranrosales23@hotmail.com>
2022-05-11 14:21:47 -0500

    add test

--- __test__/test.js
+++ __test__/test.js
@@ -1,7 +1,7 @@
 const app = require("../app");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
-
+require("dotenv").config({ path: ".env.test" });
 beforeAll((done) => {
   mongoose.connect("mongodb://localhost:27017/favTest", () => done());
 });
@@ -14,7 +14,7 @@ afterAll((done) => {
 
 let token = "";
 
-test("POST /api/user/", async () => {
+test("Create User -> POST /api/user/", async () => {
   const data = {
     email: "moranrosales23@hotmail.com",
     password: "A123456s@",
@@ -29,4 +29,137 @@ test("POST /api/user/", async () => {
       expect(response.body.data._id).toBeDefined();
       expect(response.body.data.email).toBe(data.email);
     });
+});
+
+test("Error Create User -> POST /api/user/", async () => {
+  const data = {
+    email: "moranrosales23@hotmail.com",
+  };
+
+  await supertest(app).post("/api/user/").send(data).expect(400);
+});
+
+test("Login -> POST /auth/local/login", async () => {
+  const data = {
+    email: "moranrosales23@hotmail.com",
+    password: "A123456s@",
+  };
+
+  await supertest(app)
+    .post("/auth/local/login/")
+    .send(data)
+    .expect(200)
+    .then((response) => {
+      expect(response.body.data).toBeDefined();
+      token = response.body.data;
+    });
+});
+
+test("Error Login -> POST /auth/local/login", async () => {
+  const data = {
+    email: "moranrosales23@hotmail.com",
+    password: "A123456s@-",
+  };
+  await supertest(app).post("/auth/local/login/").send(data).expect(404);
+});
+
+test("Get User -> GET /api/user/", async () => {
+  await supertest(app)
+    .get("/api/user/")
+    .set("Authorization", `Bearer ${token}`)
+    .expect(200)
+    .then(async (response) => {
+      expect(response.body.data.length).toBe(1);
+    });
+});
+
+test("Create List of Favorites -> POST /api/favs/", async () => {
+  const data = {
+    name: "Nombre de lista",
+    favs: [
+      {
+        title: "Nombre de favorito",
+        description: "descripcion del favorito",
+        link: "www.mi-favorito.com",
+      },
+      {
+        title: "Otro nombre de favorito",
+      },
+    ],
+  };
+
+  await supertest(app)
+    .post("/api/favs/")
+    .set("Authorization", `Bearer ${token}`)
+    .send(data)
+    .expect(201)
+    .then(async (response) => {
+      expect(response.body.message).toEqual("Fav created");
+      expect(response.body.data._id).toBeDefined();
+      expect(response.body.data.name).toBe(data.name);
+      expect(response.body.data.favs.length).toBe(2);
+    });
+});
+
+test("Error Create List of Favorites -> POST /api/favs/", async () => {
+  const data = {
+    name: "",
+    favs: [
+      {
+        title: "Nombre de favorito",
+        description: "descripcion del favorito",
+        link: "www.mi-favorito.com",
+      },
+      {
+        title: "Otro nombre de favorito",
+      },
+    ],
+  };
+
+  await supertest(app)
+    .post("/api/favs/")
+    .set("Authorization", `Bearer ${token}`)
+    .send(data)
+    .expect(400);
+});
+
+test("Get List of Favorites -> GET /api/favs/", async () => {
+  await supertest(app)
+    .get("/api/favs/")
+    .set("Authorization", `Bearer ${token}`)
+    .expect(200)
+    .then(async (response) => {
+      expect(response.body.data.length).toBe(1);
+    });
+});
+
+test("Delete List of Favorites -> DELETE /api/favs/:id", async () => {
+  const data = {
+    name: "Nombre de prueba 2 ",
+    favs: [
+      {
+        title: "Nombre de favorito 2",
+        description: "descripcion del favorito 2",
+        link: "www.mi-favorito.com",
+      },
+    ],
+  };
+  let id = "";
+  await supertest(app)
+    .post("/api/favs/")
+    .set("Authorization", `Bearer ${token}`)
+    .send(data)
+    .expect(201)
+    .then(async (response) => {
+      expect(response.body.message).toEqual("Fav created");
+      expect(response.body.data._id).toBeDefined();
+      expect(response.body.data.name).toBe(data.name);
+      expect(response.body.data.favs.length).toBe(1);
+      id = response.body.data._id;
+    });
+
+  await supertest(app)
+    .delete("/api/favs/" + id)
+    .set("Authorization", `Bearer ${token}`)
+    .expect(200);
 });
--- controllers/auth.js
+++ controllers/auth.js
@@ -5,7 +5,6 @@ const logIn = async (req, res) => {
     const { code, message, data } = await Auth.logIn(req.body);
     res.status(code).send({ message, data });
   } catch (error) {
-    console.log(error);
     res
       .status(400)
       .send({ message: "User o Password incorrect", errores: error });
